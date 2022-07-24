1581cb644d66c35852ff97e24dd6c5bf266da741
Alvin Thai <alvinquanthai@gmail.com>
2022-07-24 11:05:24 -0700

    Divide program into functions
    the GUI is built before calling main.  Radio buttons are made after calling main upon initiating, because the methods inside shuffle the playlist for random songs.

--- recommender.py
+++ recommender.py
@@ -1,3 +1,13 @@
+'''
+    Author: Alvin Thai
+    Per Scholas Capstone I 
+    Recommender connects with the spotify API to produce songs recommendations based 
+    on the input playlist.  Entering a playlist URL will load all songs into a 
+    dictionary.  Clicking the shuffle button picks five songs at random 
+    from the dictionary.  Clicking the recommend button will call the Spotify
+    API to recommend five songs and display them on the GUI.  
+'''
+
 import requests
 import json
 import base64
@@ -5,18 +15,61 @@ from pprint import pprint
 from dotenv import dotenv_values
 import random
 import tkinter as tk
+from tkinter import ttk
 import re
 
-config = dotenv_values('.env')
-TOKEN_URL = 'https://accounts.spotify.com/api/token'
-base_url = 'https://api.spotify.com/v1/'
+# Global variables
+s_links = {}
+random_songs = []
+radio_group = []
 headers = {}
 data = {}
 
+def connect_api(config, TOKEN_URL):
+    '''
+    Reads client id, client secret, and token url to modify global variables
+    that connect to Spotify API
+    '''
+    global headers
+    global data
+    # Encode as Base64
+    message = f"{config['client_id']}:{config['client_secret']}"
+    messageBytes = message.encode('ascii')
+    base64Bytes = base64.b64encode(messageBytes)
+    base64Message = base64Bytes.decode('ascii')
+
+    # define fields for request get 
+    headers['Authorization'] = f"Basic {base64Message}"
+    data['grant_type'] = "client_credentials"
+
+    r = requests.post(TOKEN_URL, headers=headers, data=data)
+    token = r.json()['access_token']
+
+    # Default playlist is Vangelis
+    playlistId = "37i9dQZF1E4DxWTWjR3190"
+    playlistUrl = f"https://api.spotify.com/v1/playlists/{playlistId}"
+
+    headers = {
+        "Authorization" : "Bearer " + token
+    }
+
+    res = requests.get(url=playlistUrl, headers=headers)
+    p_dict = json.loads(json.dumps(res.json(), indent=2))
+
+    # makes list of (track_name, artist, track_id, artist_id) tuples
+    names_artist = [(item['track']['name'], item['track']['album']['artists'][0]['name'],
+        item['track']['id'], item['track']['album']['artists'][0]['id'])
+        for item in p_dict['tracks']['items']]
+
+    # make a dictionary of artist name keys to artist id, track name, and track id tuples
+    global s_links
+    for song in names_artist:
+        s_links[(song[1], song[3])] = s_links.get((song[1], song[3]), []) + [(song[0], song[2])]
+
 # build GUI
 window = tk.Tk()
-frm_a = tk.Frame()
-frm_b = tk.Frame()
+frm_a = ttk.Frame()
+frm_b = ttk.Frame()
 
 lbl_a = tk.Label(
     master=frm_a,
@@ -69,7 +122,6 @@ btn_playlist = tk.Button(
     bg="blue",
     fg="yellow"
 )
-
 btn_playlist.bind("<Button-1>", playlist_click)
 
 def shuffle_click(event):
@@ -86,7 +138,7 @@ def shuffle_click(event):
             random_songs.append((random_artist, song)) 
 
     for random_artist, song in random_songs:
-        r = tk.Radiobutton(
+        r = ttk.Radiobutton(
             window,
             text = song[0] + " by " + random_artist[0],
             value = (song[1], random_artist[1]),
@@ -103,7 +155,6 @@ btn_shuffle = tk.Button(
     bg="blue",
     fg="yellow"
 )
-
 btn_shuffle.bind("<Button-1>", shuffle_click)
 
 lbl_b = tk.Label(
@@ -130,7 +181,6 @@ def rec_click(event):
         print(item['name'], item['external_urls']['spotify'])
         lbl_b['text'] += "\n" + item['name'] + " by " + str([person['name'] for person in item['artists']])
 
-
 btn_rec = tk.Button(
     master=frm_b,
     text="Recommend",
@@ -139,47 +189,9 @@ btn_rec = tk.Button(
     bg="blue",
     fg="yellow"
 )
-
 btn_rec.bind("<Button-1>", rec_click)
 
-# Encode as Base64
-message = f"{config['client_id']}:{config['client_secret']}"
-messageBytes = message.encode('ascii')
-base64Bytes = base64.b64encode(messageBytes)
-base64Message = base64Bytes.decode('ascii')
-
-# define fields for request get 
-headers['Authorization'] = f"Basic {base64Message}"
-data['grant_type'] = "client_credentials"
-
-r = requests.post(TOKEN_URL, headers=headers, data=data)
-token = r.json()['access_token']
-
-# Default playlist is Vangelis
-playlistId = "37i9dQZF1E4DxWTWjR3190"
-playlistUrl = f"https://api.spotify.com/v1/playlists/{playlistId}"
-
-headers = {
-    "Authorization" : "Bearer " + token
-}
-
-res = requests.get(url=playlistUrl, headers=headers)
-p_dict = json.loads(json.dumps(res.json(), indent=2))
-
-print(p_dict['name'] + " playlist")
-
-
-names_artist = [(item['track']['name'], item['track']['album']['artists'][0]['name'],
-    item['track']['id'], item['track']['album']['artists'][0]['id'])
-     for item in p_dict['tracks']['items']]
-
-# make a dictionary of artist name keys to artist id, track name, and track id tuples
-s_links = {}
-for song in names_artist:
-    s_links[(song[1], song[3])] = s_links.get((song[1], song[3]), []) + [(song[0], song[2])]
-for k, v in s_links.items():
-    print(k, v)
-
+# Pack widgets onto GUI
 frm_a.pack(fill=tk.BOTH, expand=True)
 ent_playlist.pack()
 ent_playlist.insert(0, "Playlist link here")
@@ -188,19 +200,24 @@ frm_b.pack(fill=tk.BOTH, expand=True)
 btn_rec.pack(fill=tk.BOTH, expand=True)
 btn_shuffle.pack(fill=tk.BOTH, expand=True)
 
+def main():
+    config = dotenv_values('.env')
+    TOKEN_URL = 'https://accounts.spotify.com/api/token'
+    connect_api(config, TOKEN_URL)
 
-# Instance radio buttons
+if __name__ == '__main__':
+    main()
+
+# Recommends random songs from playlist at the end of frame b
 selected_song = tk.StringVar()
-random_songs = []
 while len(random_songs) < 5:
     random_artist = random.choice(list(s_links))
     song = random.choice(s_links[random_artist])
     if not (random_artist, song) in random_songs:
         random_songs.append((random_artist, song)) 
 
-radio_group = []
 for random_artist, song in random_songs:
-    r = tk.Radiobutton(
+    r = ttk.Radiobutton(
         window,
         text = song[0] + " by " + random_artist[0],
         value = (song[1], random_artist[1]),
@@ -208,13 +225,5 @@ for random_artist, song in random_songs:
     )
     radio_group.append(r)
     r.pack(fill='x', padx=5, pady=5)
-
-# Recommend songs based on default playlist
-res = requests.get('https://api.spotify.com/v1/recommendations?limit=5&market=ES&seed_artists='
-+ random_artist[1] + '&seed_genres=''&seed_tracks=' + song[1], headers=headers)
-recommendations = json.loads(json.dumps(res.json(), indent=2))
-pprint(recommendations['tracks'][0])
-for item in recommendations['tracks']:
-    print(item['name'], item['external_urls']['spotify'])
 
 window.mainloop()
